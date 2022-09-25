c6746910ba995c4b147d7c6870609a9433a77929
Zak-aden1 <85500942+Zak-aden1@users.noreply.github.com>
2022-09-25 22:32:11 +0100

    Time: 135 ms (27.21%), Space: 43.2 MB (99.09%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,16 +4,18 @@
  */
 var isPalindrome = function(s) {
     const str = s.replace(/[\W_]+/g, '').toLowerCase()
-    let leftIndex = 0
-    let rightIndex = str.length - 1
+    let min = 0
+    let max = str.length -1
     
-    while(leftIndex < rightIndex) {
-        if(str[leftIndex] !== str[rightIndex]) return false
-        leftIndex ++
-        rightIndex --
+    while(min < max) {
+        if(str[min] !== str[max]) {
+            return false
+        } else {
+            min ++
+            max --
+        }
     }
     return true
-    
 };
 
 // const cleanStr = (str) => str.replace('/[^\w]/g', '')
