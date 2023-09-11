62bb85711824360416eb4f8dac9fc173c679ba9c
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2023-09-11 15:47:37 +0100

    Time: 65 ms (56.97%) | Memory: 45.1 MB (48.01%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,14 +11,12 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if(!head) return false
     let slow = head
     let fast = head
 
-    while(fast.next && fast.next.next) {
+    while(fast && fast.next) {
         slow = slow.next
         fast = fast.next.next
-
         if(slow === fast) return true
     }
 
