0159b8a5bbe13ad88e2fae07d37f85c47772f323
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2023-09-15 16:51:48 +0100

    Time: 52 ms (79.91%) | Memory: 44.5 MB (93.11%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -5,16 +5,16 @@
  */
 var search = function(nums, target) {
     let left = 0
-    let right = nums.length -1
+    let right = nums.length -1 
 
     while(left <= right) {
         let midpoint = Math.floor((left + right) / 2)
 
-        if(nums[midpoint] === target) return midpoint 
-        
+        if(nums[midpoint] === target) return midpoint
+
         if(nums[midpoint] > target) {
-            right = midpoint - 1
-        } else if(nums[midpoint] < target) {
+            right = midpoint -1
+        } else {
             left = midpoint + 1
         }
     }
