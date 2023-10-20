663f904811955e38359c1cb2e6b5ecd6b70c33b7
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2023-10-20 17:06:28 +0100

    Time: 49 ms (97.47%) | Memory: 45.8 MB (89.75%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root, min = null, max = null) {
+    if(min !== null && root.val <= min) return false
+    if(max !== null && root.val >= max) return false
+
+    if(root.left && !isValidBST(root.left, min, root.val)) return false
+    if(root.right && !isValidBST(root.right, root.val, max)) return false
+
+    return true
+};
