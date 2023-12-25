190fa68d374aef0b84293adc1a5d3cd256dfd31e
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2023-12-25 15:28:24 +0000

    Time: 64 ms (11.06%) | Memory: 44.3 MB (50.07%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -10,18 +10,14 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    // store next in a temp var
-    // point next behind
-    // and move forward down the chain
-
     let prev = null
-    let cur = head
+    let curr = head
 
-    while(cur) {
-        let tmp = cur.next
-        cur.next = prev
-        prev = cur
-        cur = tmp
+    while (curr) {
+        let temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = temp
     }
 
     return prev
