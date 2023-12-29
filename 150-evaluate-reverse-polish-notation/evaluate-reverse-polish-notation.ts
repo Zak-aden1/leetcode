9f7c29b0c7ff7701aa89d0a1fe3a27f86d94977f
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2023-12-29 23:30:35 +0000

    Time: 65 ms (59.97%) | Memory: 45.9 MB (49.31%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.ts
@@ -0,0 +1,23 @@
+function evalRPN(tokens: string[]): number {
+    const stack = []
+
+    for (let char of tokens) {
+        if(char == '+') {
+            stack.push(stack.pop() + stack.pop())
+        } else if (char == '-') {
+            let a = stack.pop() 
+            let b = stack.pop()
+            stack.push(b - a)
+        } else if (char == '*') {
+            stack.push(stack.pop() * stack.pop())
+        } else if (char == '/') {
+            let a = stack.pop() 
+            let b = stack.pop()
+            stack.push(b / a | 0)
+        } else {
+            stack.push(+char)
+        }
+    }
+
+    return stack[0]
+};
