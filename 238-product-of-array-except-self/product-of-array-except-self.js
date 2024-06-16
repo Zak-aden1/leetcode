dc2ce639af8d98ca53fcbb1ae42ea509a756eb78
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-06-16 14:25:07 +0100

    Time: 104 ms (56.60%) | Memory: 64.5 MB (48.71%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -7,21 +7,26 @@ var productExceptSelf = function(nums) {
     let suff = []
     pre[0] = 1
     suff[nums.length - 1] = 1;
+       let ans = new Array(nums.length).fill(1)
 
-    for(let i = 1; i < nums.length; i++ ) {
-        pre[i] = pre[i - 1] * nums[i - 1]
+    let curr = 1
+    for(let i = 0; i < nums.length; i++ ) {
+        // pre[i] = pre[i - 1] * nums[i - 1]
+        ans[i] = ans[i] * curr
+        curr = curr * nums[i]
     }
 
-    for(let i = nums.length -2; i >= 0; i--) {
-        suff[i] = suff[i + 1] * nums[i + 1]
+    curr = 1
+    for(let i = nums.length - 1; i >= 0; i--) {
+        // suff[i] = suff[i + 1] * nums[i + 1]
+        ans[i] = ans[i] * curr
+        curr = curr * nums[i]
     }
 
-    let ans = []
+    // for(let i = 0; i < nums.length; i++) {
+    //     ans[i] = pre[i] * suff[i]
+    // }
 
-    for(let i = 0; i < nums.length; i++) {
-        ans[i] = pre[i] * suff[i]
-    }
-
-    console.log(pre, suff)
+    // console.log(pre, suff)
     return ans
 };
