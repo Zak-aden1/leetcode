8f5dfad3d4df7fd87197f3960e131bfb4d298575
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-06-17 13:47:39 +0100

    Time: 95 ms (7.05%) | Memory: 54.3 MB (48.60%) - LeetSync

--- 57-insert-interval/insert-interval.js
+++ 57-insert-interval/insert-interval.js
@@ -4,6 +4,12 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
+    // loop through intervals
+    // newInterval end < interval[0]
+        // add in new interval and return
+    // newINterval end > interval[1]
+        // add inteval to new arr and continue
+    // update newInterval with new minmax
     const output = []
 
     for(let i = 0; i < intervals.length; i++) {
@@ -16,7 +22,7 @@ var insert = function(intervals, newInterval) {
         } else {
             newInterval = [
                 Math.min(newInterval[0], intervals[i][0]),
-                Math.max(newInterval[1], intervals[i][1])
+                Math.max(newInterval[1], intervals[i][1]),
             ]
         }
     }
