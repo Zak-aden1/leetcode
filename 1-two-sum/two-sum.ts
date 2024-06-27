0dec0262c10ab0809e69e627c4ce7fea7fd168b1
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-06-27 21:43:35 +0100

    Time: 53 ms (95.60%) | Memory: 52.6 MB (43.27%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,19 +1,12 @@
 function twoSum(nums: number[], target: number): number[] {
-    // create a hash map
-    // loop through list
-    // check if num - target = key in map
-    // add to map with num as key and val is how many
     const map = {}
 
     for(let i = 0; i < nums.length; i++) {
-        const el = nums[i]
-        const comPair = target - el
-        if(map[comPair] == null) {
-            map[el] = i
+        let pair = target - nums[i]
+        if(map[pair] === undefined) {
+            map[nums[i]] = i
         } else {
-            return [map[comPair], i]
+            return [i, map[pair]]
         }
     }
-
-
 };
