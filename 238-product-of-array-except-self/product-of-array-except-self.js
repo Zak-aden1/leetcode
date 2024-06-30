8ffcc8b4fedbecf88edc23256868c1b0243cd37c
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-06-30 19:00:12 +0100

    Time: 111 ms (37.81%) | Memory: 64.1 MB (57.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,30 +3,22 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let pre = []
-    let suff = []
-    pre[0] = 1
-    suff[nums.length - 1] = 1;
-       let ans = new Array(nums.length).fill(1)
+    let output = []
+
 
     let curr = 1
-    for(let i = 0; i < nums.length; i++ ) {
-        // pre[i] = pre[i - 1] * nums[i - 1]
-        ans[i] = ans[i] * curr
+    for(let i = 0; i < nums.length; i++) {
+        output.push(curr)
+        // output[i] = curr
         curr = curr * nums[i]
     }
 
     curr = 1
+    console.log(output)
     for(let i = nums.length - 1; i >= 0; i--) {
-        // suff[i] = suff[i + 1] * nums[i + 1]
-        ans[i] = ans[i] * curr
+        output[i] = output[i] * curr
         curr = curr * nums[i]
     }
 
-    // for(let i = 0; i < nums.length; i++) {
-    //     ans[i] = pre[i] * suff[i]
-    // }
-
-    // console.log(pre, suff)
-    return ans
+    return output
 };
