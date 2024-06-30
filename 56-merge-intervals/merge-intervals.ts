c92e7d19648e05a0be3e9b57b92b82438c67779d
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-06-30 19:22:34 +0100

    Time: 88 ms (88.63%) | Memory: 58.1 MB (95.02%) - LeetSync

--- 56-merge-intervals/merge-intervals.ts
+++ 56-merge-intervals/merge-intervals.ts
@@ -1,14 +1,14 @@
 function merge(intervals: number[][]): number[][] {
     intervals = intervals.sort((a, b) => a[0] - b[0])
-    let output = []
+    const output = []
     let prev = intervals[0]
 
-    for (let i = 1; i < intervals.length; i++) {
-        if(prev[1] >= intervals[i][0]) {
-            prev[1] = Math.max(prev[1], intervals[i][1])
-        } else {
+    for(let i = 1; i < intervals.length; i++) {
+        if(prev[1] < intervals[i][0]) {
             output.push(prev)
             prev = intervals[i]
+        }  else {
+            prev[1] = Math.max(prev[1], intervals[i][1])
         }
     }
 
