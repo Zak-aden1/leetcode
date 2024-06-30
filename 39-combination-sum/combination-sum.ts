a96c2deec036df3169f7353291fa212ccd729615
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-06-30 19:08:00 +0100

    Time: 71 ms (91.14%) | Memory: 54.7 MB (71.84%) - LeetSync

--- 39-combination-sum/combination-sum.ts
+++ 39-combination-sum/combination-sum.ts
@@ -1,21 +1,20 @@
 function combinationSum(candidates: number[], target: number): number[][] {
-    let res = []
+    let output = []
 
     const dfs = (i, curr, total) => {
-        if (total === target) {
-            res.push([...curr])
+        if(target === total) {
+            output.push([...curr])
             return
         }
-        if (i > candidates.length -1 || total > target) return
+        if(total > target || i > candidates.length -1) return
 
-        // recurse with same number
         curr.push(candidates[i])
         dfs(i, curr, total + candidates[i])
-        // recurse with next number
         curr.pop()
         dfs(i + 1, curr, total)
     }
+
     dfs(0, [], 0)
 
-    return res
+    return output
 };
