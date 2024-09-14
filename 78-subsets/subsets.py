5932f81592ee9ec56e9890f15ea0aa367a73b24a
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-09-14 12:32:29 +0100

    Time: 41 ms (26.11%) | Memory: 16.7 MB (24.78%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,18 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # backtracking
-        # 
+        # decision tree
+        # with num[i] and without
+        # increment i
         arr = []
 
-        def dfs(i, lists):
-            if i == len(nums):
+        def bt(i, lists):
+            if i >= len(nums):
                 arr.append(lists.copy())
                 return
-            
             lists.append(nums[i])
-            dfs(i + 1, lists)
+            bt(i+1, lists)
             lists.pop()
-            dfs(i + 1, lists)
-        
-        dfs(0, [])
+            bt(i+1, lists)
+        bt(0, [])
+
         return arr
