d2716b12803ee66edd3aba4f7d7f07a9bf6089b6
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-09-15 14:32:13 +0100

    Time: 45 ms (8.08%) | Memory: 16.7 MB (24.45%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,18 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # decision tree
-        # with num[i] and without
-        # increment i
         arr = []
 
         def bt(i, lists):
-            if i >= len(nums):
+            if i == len(nums):
                 arr.append(lists.copy())
                 return
+            
             lists.append(nums[i])
-            bt(i+1, lists)
+            bt(i+ 1, lists)
             lists.pop()
-            bt(i+1, lists)
+            bt(i + 1, lists)
         bt(0, [])
 
         return arr
