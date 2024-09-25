c4dc9ee80e857f2f54c498f86555c7fce99d8813
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-09-25 22:13:29 +0100

    Time: 80 ms (55.42%) | Memory: 62.5 MB (48.43%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,12 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    nums = nums.sort((a, b) => a - b)
+    const set = new Set()
 
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] === nums[i+1]) {
-            return true
-        }
+    for (let num of nums) {
+        if(set.has(num)) return true
+        set.add(num)
     }
 
     return false
