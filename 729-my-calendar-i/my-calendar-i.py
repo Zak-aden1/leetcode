805d1db78e921369f36ed17e3f2de53a32530435
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-09-29 16:07:16 +0100

    Time: 318 ms (37.06%) | Memory: 17.2 MB (91.05%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -4,12 +4,12 @@ class MyCalendar:
         self.calendar = []
 
     def book(self, start: int, end: int) -> bool:
-        for s, e in self.calendar:
-            if e > start and s < end:
+        for [s, e] in self.calendar:
+            if s < end and e > start: # o-----o o-----o
                 return False
+        
         self.calendar.append([start, end])
         return True
-        
 
 
 # Your MyCalendar object will be instantiated and called as such:
