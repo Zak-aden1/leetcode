68c656003ee9dba08d13d3add3b46f2a31fb27ea
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-10-05 23:21:31 +0100

    Time: 28 ms, Memory: 11.6 MB - LeetHub

--- sqrtx/sqrtx.py
+++ sqrtx/sqrtx.py
@@ -6,17 +6,16 @@ class Solution(object):
         """
         l, r = 0, x
         res = 0
-        
         while l <= r:
             m = l + ((r - l) // 2)
             
+            if m**2 == x:
+                return m
+            
             if m**2 > x:
                 r = m - 1
-            elif m**2 < x:
+            else:
                 res = m
                 l = m + 1
-            else:
-                return m
         
         return res
-        
