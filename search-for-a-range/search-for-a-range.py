1785e9660c85671e5f7e0a6c37c35eb4e9dab4b6
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-10-06 16:34:26 +0100

    Time: 85 ms, Memory: 17.8 MB - LeetHub

--- search-for-a-range/search-for-a-range.py
+++ search-for-a-range/search-for-a-range.py
@@ -1,31 +1,24 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l, r = 0, len(nums) -1
+        left = self.binSearch(nums, target, True)
+        right = self.binSearch(nums, target, False)
         
-        idx = None
+        return [left, right]
+        
+    def binSearch(self, nums, target, leftBias):
+        l, r = 0, len(nums) - 1
+        i = -1
         while l <= r:
             m = l + ((r - l) // 2)
             
-            if nums[m] == target:
-                idx = m
-                break
-            
             if target > nums[m]:
                 l = m + 1
-            else:
+            elif target < nums[m]:
                 r = m - 1
-        
-        if idx == None:
-            return [-1, -1]
-        
-        ans = [idx, idx]
-        l, r = idx, idx
-        while l > 0 and nums[l] == nums[l - 1]:
-            l -= 1
-            ans[0] = l
-            
-        while r < len(nums) -1 and nums[r] == nums[r + 1]:
-            r += 1
-            ans[1] = r
-        
-        return ans
+            else:
+                i = m
+                if leftBias:
+                    r = m -1
+                else:
+                    l = m + 1
+        return i 
