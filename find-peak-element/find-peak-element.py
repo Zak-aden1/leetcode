89b7067b0117d075ddeaf27f618c626747c13b0a
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-10-06 11:51:45 +0100

    Time: 34 ms, Memory: 11.8 MB - LeetHub

--- find-peak-element/find-peak-element.py
+++ find-peak-element/find-peak-element.py
@@ -7,12 +7,12 @@ class Solution(object):
         l, r = 0, len(nums) - 1
         
         while l <= r:
-            m = l + ((r - l) // 2)
+            m = l + ((r - l) //2)
             
-            # if left i greater
             if m > 0 and nums[m - 1] > nums[m]:
                 r = m - 1
             elif m < len(nums) - 1 and nums[m + 1] > nums[m]:
                 l = m + 1
             else:
                 return m
+                
