b8769d9fc50cfd39817bef1c390d5fdbadc12417
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-10-11 23:22:26 +0100

    Time: 417 ms (10.96%) | Memory: 96.4 MB (26.57%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.ts
+++ 1023-time-based-key-value-store/time-based-key-value-store.ts
@@ -6,24 +6,29 @@ class TimeMap {
     }
 
     set(key: string, value: string, timestamp: number): void {
+        // if undefined set value to empty array
         if (!this.map[key]) this.map[key] = []
 
+        // push new additions to the array
         this.map[key].push([value, timestamp]) 
         
     }
 
     get(key: string, timestamp: number): string {
+        // if nullish return empty string
         if (!this.map[key]) return ""
 
         let left = 0
         let right = this.map[key].length - 1
 
         let res = ""
+        // binary search for (logn) time
         while (left <= right) {
             const m = left + (Math.floor((right - left) / 2))
 
             const [val, stamp] = this.map[key][m]
             if (stamp <= timestamp) {
+                // update val as this is the closest to timestamp we've seen
                 res = val
                 left = m + 1
             } else {
