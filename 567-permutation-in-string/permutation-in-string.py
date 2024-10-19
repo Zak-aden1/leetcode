3ba0b26849d94b9db46c041b4039ec0468789356
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-10-19 14:13:17 +0100

    Time: 15 ms (100.00%) | Memory: 16.6 MB (71.52%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,26 +1,31 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n1 = len(s1)
-        n2 = len(s2)
-
-        s1_count = [0] * 26
-        s2_count = [0] * 26
-
-        if n1 > n2:
+        if s1 == "":
+            return True
+        if s2 == "":
             return False
 
-        for i in range(n1):
-            s1_count[ord(s1[i]) - ord("a")] += 1
-            s2_count[ord(s2[i]) - ord("a")] += 1
-        
-        if s1_count == s2_count:
+        s1_map = [0] * 26
+        s2_map = [0] * 26
+
+        if len(s1) > len(s2):
+            return False
+
+        for i in range(len(s1)):
+            s1_map[ord(s1[i]) - ord("a")] += 1
+            s2_map[ord(s2[i]) - ord("a")] += 1
+
+        if s1_map == s2_map:
             return True
         
+        for i in range(len(s1), len(s2)):
+            # rm prev
+            s2_map[ord(s2[i - len(s1)]) - ord("a")] -= 1
 
-        for i in range(n1, n2):
-            s2_count[ord(s2[i]) - ord("a")] += 1
-            s2_count[ord(s2[i - n1]) - ord("a")] -= 1
-            if s1_count == s2_count:
+            # add
+            s2_map[ord(s2[i]) - ord("a")] += 1
+            if s1_map == s2_map:
                 return True
         
         return False
+
