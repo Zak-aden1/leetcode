37c6d5c2bdd6a55727d9c9f0e6765c3cb2c4b39b
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-11-24 19:06:56 +0000

    Time: 8 ms (91.01%) | Memory: 54.6 MB (54.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.ts
+++ 242-valid-anagram/valid-anagram.ts
@@ -1,5 +1,24 @@
 function isAnagram(s: string, t: string): boolean {
-    s = s.split("").sort().join("")
-    t = t.split("").sort().join("")
-    return s === t
+    let s_map = Counter(s)
+    let t_map = Counter(t)
+
+    if (Object.keys(s_map).length != Object.keys(t_map).length) {
+        return false
+    }
+
+    for (let key in s_map) {
+        if (s_map[key] !== t_map[key]) return false
+    }
+
+    return true
 };
+
+
+function Counter(s) {
+    const map = {}
+
+    for (let char of s) {
+        map[char] = map[char] + 1 || 1
+    }
+    return map
+}
