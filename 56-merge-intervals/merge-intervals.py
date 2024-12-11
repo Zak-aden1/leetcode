e112e2e013e27e05996057b00adc4ad07dca6149
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2024-12-11 10:09:20 +0000

    Time: 13 ms (14.33%) | Memory: 20.6 MB (42.50%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,17 +1,22 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key = lambda x: x[0])
+        # loop through arr
+        # if intervals[i] overlap merge
+        # else add intevals[i]
+        if len(intervals) == 0:
+            return intervals
+        
+        intervals.sort()
 
-        res = []
+        arr = [intervals[0]]
 
         for i in range(1, len(intervals)):
-            if intervals[i - 1][1] < intervals[i][0]:
-                res.append(intervals[i - 1])
-            else:
-                intervals[i] = [
-                    min(intervals[i - 1][0], intervals[i][0]),
-                    max(intervals[i - 1][1], intervals[i][1])
+            if arr[-1][1] >= intervals[i][0]:
+                arr[-1] = [
+                    min(arr[-1][0], intervals[i][0]),
+                    max(arr[-1][1], intervals[i][1])
                 ]
-        
-        res.append(intervals[len(intervals) - 1])
-        return res
+            else:
+                arr.append(intervals[i])
+
+        return arr
