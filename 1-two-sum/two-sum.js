c2cf7dfc8703fa28b2647eaf9ac91d2d87d06f19
Zak Aden <85500942+Zak-aden1@users.noreply.github.com>
2025-02-16 22:02:30 +0000

    Time: 49 ms (29.25%) | Memory: 48.9 MB (98.10%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,15 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = {}
-
-    for(let i = 0; i < nums.length; i++) {
-        const comPair = target - nums[i]
-
-        if(map[comPair] === undefined) {
-            map[nums[i]] = i
-        } else {
-            return [i, map[comPair]]
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) {
+                return [i, j]
+            }
         }
     }
+    
 };
